a6f729aacf9653d69f32080eb18878bd6575167e
maciej <m.p.dutkiewicz@gmail.com>
2017-07-06 17:15:43 +0200

    poprawione ; i deklaracja

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 var a = prompt('Wartość zmiennej a dla równania (a*a)+(2*a*b)-(b*b)'),
 	b = prompt('Wartość zmiennej b dla równania (a*a)+(2*a*b)-(b*b)'),
 	value = (a*a)+(2*a*b)-(b*b);
-console.log('Podstawiamy do poniższego równania zmienne ' + 'a = '+ a + ' i ' + 'b = ' + b)
+console.log('Podstawiamy do poniższego równania zmienne ' + 'a = '+ a + ' i ' + 'b = ' + b);
 console.log('Wynik równania ' + 'value = (a*a)+(2*a*b)-(b*b) ' + '= ' + value);
 if (value>0) {
-	console.log('Wynik dodatni')
+	console.log('Wynik dodatni');
 } else if (value<0) {
-	console.log('Wynik ujemny')
+	console.log('Wynik ujemny');
 } else {
+	console.log('Wynik niedodatni oraz nieujemny')
+}
+switch (value) {
+	case 0:
 	console.log('Wynik równy zero')
 }
